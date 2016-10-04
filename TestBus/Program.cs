47464ecd7d8d5753f48c78a449cd57e24b43b73e
Richard Nagle <richard.nagle@firsthelpline.com>
2016-10-04 12:02:13 +0100

    initial commit

---
+++ TestBus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Fri.Xhl.Domain.Events.Interfaces;
+using MassTransit;
+
+namespace TestBus
+{
+    class Program
+    {
+        static void Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+                var host = x.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                x.ReceiveEndpoint(host, "test_bus", endpoint =>
+                {
+                    endpoint.Handler<IPostCodeProvidedEvent>(async context =>
+                    {
+                        await Console.Out.WriteLineAsync($"{DateTime.Now.ToShortTimeString()} Received Postcode: {context.Message.PostCode}");
+                    });
+                });
+            });
+
+            bus.Start();
+
+        }
+    }
+}
