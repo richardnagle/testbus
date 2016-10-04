d78b06c91bfc3755ce3542bf4cb1db4c7a0bbfb1
Richard Nagle <richard.nagle@firsthelpline.com>
2016-10-04 16:31:03 +0100

    Add SendCommands project to test sending commands

---
+++ SendCommand/DistributedBus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace SendCommand
+{
+    public static class DistributedBus
+    {
+        private static readonly IBusControl _bus;
+
+        static DistributedBus()
+        {
+            _bus = Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+                x.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+        }
+
+        public static void Stop()
+        {
+            _bus.Stop();
+        }
+
+        public static void Start()
+        {
+            _bus.Start();
+        }
+
+        public static async Task Send<T>(object command)
+            where T : class
+        {
+            var address = new Uri("rabbitmq://localhost/test_bus");
+            var consumerEndpoint = await _bus.GetSendEndpoint(address);
+            await consumerEndpoint.Send<T>(command);
+        }
+    }
+}
---
+++ SendCommand/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TestBus.Commands;
+
+namespace SendCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DistributedBus.Start();
+
+            do
+            {
+                Console.WriteLine("Enter message (or quit to exit)");
+                Console.Write("> ");
+                string value = Console.ReadLine();
+
+                if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                DistributedBus.Send<IHelloWorld>(new {Name = value, Age = 21}).Wait();
+            }
+
+            while (true); DistributedBus.Stop();
+        }
+    }
+}
